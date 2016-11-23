e6ad831c1b0b82fa99746cd51909f17fa72d7d06
Alex <alexander.y.li2000@gmail.com>
2016-11-23 15:50:34 -0500

    Add Twitter and FB Auth

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  oauth: {
+   twitter: {
+     consumer_key: "hxSv6tiOIphs6W7vSTbefNxna", // REQUIRED
+     consumer_secret: "HrTB3xExLVaWbnlpMV1eancnOCa6pb2jjOTfAHBwSznDuKQhnJ" // REQUIRED
+   },
+   facebook: {
+     appIds: "340650776310380"
+   }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
