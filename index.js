fcb9641161d6d4798699a79c4680bb2895a14dcf
Alex <alexli2000@users.noreply.github.com>
2017-03-01 22:48:14 -0500

    Add push dev link

--- index.js
+++ index.js
@@ -18,10 +18,16 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || 'Orca', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   push: {
-    ios: {
+    ios: [
+     {
       pfx: 'OrcaPushProd.p12',
       bundleId: 'com.alexli.Orka',
       production: true
+     },
+     {
+      pfx: 'OrcaPushDev.p12',
+      bundleId: 'com.alexli.Orka',
+      production: false
     }
   }
 });
