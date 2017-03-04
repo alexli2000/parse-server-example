6783d579e1e62a2b7b24ee3bc3826cf717ae5a1f
Alex <alexander.y.li2000@gmail.com>
2017-03-04 16:02:43 -0500

    Typo

--- cloud/main.js
+++ cloud/main.js
@@ -5,19 +5,13 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define('sendNewMessagePush', function(req, res) {
   // request has 2 parameters: params passed by the client and the authorized user
-    var params = request.params;
+    var params = req.params;
     //var user = request.user;
     console.log(1);
     // extract out the channel to send
     //var action = params.action;
     var message = params.message;
     //var customData = params.customData;
-    console.log(2);
-    // use to custom tweak whatever payload you wish to send
-    var pushQuery = new Parse.Query(Parse.Installation);
-    pushQuery.equalTo("channels", "global");
-
-    console.log(3);
 
     Parse.Push.send({
   channels: ['global'],
