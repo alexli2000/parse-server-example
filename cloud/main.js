4d28cea73f2a33d78bbb366d842284fd2257ffb6
Alex <alexander.y.li2000@gmail.com>
2017-03-04 15:54:46 -0500

    Fixes

--- cloud/main.js
+++ cloud/main.js
@@ -7,35 +7,30 @@ Parse.Cloud.define('sendNewMessagePush', function(req, res) {
   // request has 2 parameters: params passed by the client and the authorized user
     var params = request.params;
     //var user = request.user;
-
+    console.log(1);
     // extract out the channel to send
     //var action = params.action;
     var message = params.message;
     //var customData = params.customData;
-
+    console.log(2);
     // use to custom tweak whatever payload you wish to send
     var pushQuery = new Parse.Query(Parse.Installation);
-    pushQuery.equalTo("deviceType", "ios");
+    pushQuery.equalTo("channels", "global");
 
-    var payload = {
-      "channels": [
-                "global"
-              ],
-      "data": {
-        "alert": message,
-        "badge": increment
-      }
-    };
-
-    // Note that useMasterKey is necessary for Push notifications to succeed.
+    console.log(3);
 
     Parse.Push.send({
-    where: pushQuery,      // for sending to a specific channel                                                                                                                                 data: payload,
-    }, { success: function() {
-       console.log("#### PUSH OK");
-    }, error: function(error) {
-       console.log("#### PUSH ERROR" + error.message);
-    }, useMasterKey: true});
+  channels: ['global'],
+  data: {
+    alert: message,
+    badge: 'increment',
+    sound: 'default'
+  }
+}, { useMasterKey: true }).then(() => {
+  console.log('Push ok');
+}, (e) => {
+  console.log('Push error', e);
+});
 
     response.success('success');
 });
