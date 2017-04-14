673e345ff0aa34e50e67ef2642f88e7a6a7d4286
Alex Li <alexander.y.li2000@gmail.com>
2017-04-14 16:18:10 -0400

    Add userId to push

--- cloud/main.js
+++ cloud/main.js
@@ -5,25 +5,26 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('sendNewMessagePush', function(req, res) {
   // request has 2 parameters: params passed by the client and the authorized user
     var params = req.params;
-    //var user = request.user;
+    var userId = params.userId;
     var title = params.title;
-    var message = params.message;
+    var message = params.body;
     var channel = params.channel;
     var pushType = params.type;
 
     var query = new Parse.Query(Parse.Installation);
     query.equalTo('channels', channel);
     query.notEqualTo('isSilenced', true);
-    
+
     Parse.Push.send({
       where: query,
       data: {
         alert: {
           title: title,
-          body: message,
-          type: pushType,
-          objectId: channel
-        }
+          body: message
+        },
+        type: pushType,
+        objectId: channel,
+        userId: userId
       }
     }, { useMasterKey: true }).then(() => {
       res.success('Sent message');
